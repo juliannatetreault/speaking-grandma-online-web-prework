1092741fa5d3838063a6b4a8f844e77692c65979
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-09 04:02:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(string)
   if string == string.upcase
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif string ==  
+  elsif string == 
     return "NOT SINCE 1938!"
   else string == string.downcase
     return "HUH?! SPEAK UP, SONNY!"
